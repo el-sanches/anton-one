70f723c5695c820d00fe2f139a8aef9ac7fa19b1
Alexander Danilov <alxx.danilov@gmail.com>
2021-09-26 03:59:42 +0300

    add anton build

---
+++ php/config.php
@@ -0,0 +1,17 @@
+<?php
+  return [
+    // Адрес и версия сайта
+    'site'     => 'antonantonov.com',
+    'site_url' => 'https://antonantonov.com/',
+    'version'  => filemtime( $_SERVER['DOCUMENT_ROOT'] . "/index.php" ),
+
+    // Контактный телефон
+    'phone' => 'https://wa.me/79296041777?text=' . urlencode('Здравствуйте! Интересует информация по личной работе с Антоном Владимировичем. Спасибо!'),
+
+    // Ссылки на соцсети
+    'whatsapp'        => 'https://wa.me/79296041777?text=' . urlencode('Здравствуйте! Интересует информация по личной работе с Антоном Владимировичем. Спасибо!'),
+    'instagram'       => 'https://www.instagram.com/antonov/',
+    'instagram_group' => 'https://www.instagram.com/antonantonovgroup/',
+    'youtube'         => 'https://youtu.be/PcK8VyrcphU',
+    'whatsapp_course' => 'https://wa.me/79296041777?text=' . urlencode('Добрый день! Расскажите, пожалуйста, про запись на курс...'),
+  ];
